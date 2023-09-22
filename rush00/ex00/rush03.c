ccb459d239bb94a64c376fba196b458c1aab108d
gabref <gabrielfranzer@gmail.com>
2023-09-22 16:58:11 -0300

    rush00

---
+++ rush00/ex00/rush03.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void ft_putchar(char c);
+
+void ft_print_row(int columns, char middle, char start, char end)
+{
+	int i;
+
+	ft_putchar(start);
+	i = 1;
+	while (i++ < columns - 1)
+		ft_putchar(middle);
+	if (columns > 1)
+		ft_putchar(end);
+	ft_putchar('\n');
+}
+
+void rush(int x, int y)
+{
+	int i;
+
+	i = 0;
+	while (i < y && x > 0)
+	{
+		if (i == 0 || i == y - 1)
+			ft_print_row(x, 'B', 'A', 'C');
+		else 
+			ft_print_row(x, ' ', 'B', 'B');
+		i++;
+	}
+	ft_putchar('\n');
+}
---
+++ rush00/ex00/rush04.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void ft_putchar(char c);
+
+void ft_print_row(int columns, char middle, char start, char end, int is_not_first)
+{
+	int i;
+
+	if (!is_not_first)
+		ft_putchar(start);
+	else
+		ft_putchar(end);
+
+	i = 1;
+	while (i++ < columns - 1)
+		ft_putchar(middle);
+	if (columns > 1)
+	{
+		if (!is_not_first)
+			ft_putchar(end);
+		else
+			ft_putchar(start);
+	}
+	ft_putchar('\n');
+}
+
+void rush(int x, int y)
+{
+	int i;
+
+	i = 0;
+	while (i < y && x > 0)
+	{
+		if (i == 0 || i == y - 1)
+			ft_print_row(x, 'B', 'A', 'C', i);
+		else 
+			ft_print_row(x, ' ', 'B', 'B', i);
+		i++;
+	}
+	ft_putchar('\n');
+}

9986b9551c05e3e0a31b22b88ac68d728e1e1691
gabref <gabrielfranzer@gmail.com>
2023-09-29 09:57:12 +0200

    fixed ex12

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,10 +6,11 @@
 /*   By: galves-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 04:01:59 by galves-f          #+#    #+#             */
-/*   Updated: 2023/09/27 09:17:49 by galves-f         ###   ########.fr       */
+/*   Updated: 2023/09/29 09:56:53 by galves-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
 
 typedef unsigned char	t_byte;
@@ -105,10 +106,30 @@ void	*ft_print_memory(void *addr, unsigned int size)
 	int	limit;
 
 	limit = 16;
-	if (size == 1)
+	if (size == 1 || size == 0 || addr == NULL)
 		return (addr);
 	ft_print_address_hex(addr);
 	ft_print_str_hex(addr, size, limit);
 	ft_print_content_dot(addr, size, limit);
 	return (addr);
+}
+
+int	main(void)
+{
+	char	empty[] = "";
+	char	str0[] = "Bonjour les amin----";
+	char	str1[] = "ches\t\n\tc  est fo----";
+	char	str2[] = "u\ttout\tce qu on ----";
+	char	str3[] = "peut faire avec\t----";
+	char	str4[] = "\n\tprint_memory\n\n----";
+	char 	str5[] = "\n\tlol.lol\n \0";
+
+	ft_print_memory(NULL, sizeof(NULL));
+	ft_print_memory((void *)empty, sizeof(empty));
+	ft_print_memory((void *)str0, sizeof(str0));
+	ft_print_memory((void *)str1, sizeof(str1));
+	ft_print_memory((void *)str2, sizeof(str2));
+	ft_print_memory((void *)str3, sizeof(str3));
+	ft_print_memory((void *)str4, sizeof(str4));
+	ft_print_memory((void *)str5, sizeof(str5));
 }

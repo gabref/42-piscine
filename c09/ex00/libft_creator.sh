3c2735ab9800f193c11f3c351fc83335ac5af0fa
gabref <gabrielfranzer@gmail.com>
2023-10-10 06:38:39 -0300

    fixes for c09? maybe

--- c09/ex00/libft_creator.sh
+++ c09/ex00/libft_creator.sh
@@ -1,3 +1,5 @@
-find . -type f -name "*.c" -exec gcc -Wall -Wextra -Werror -c {} \;
-ar rcs libft.a *.o
+rm -f libft.a
+find . -type f -name "*.c" -exec cc -Wall -Wextra -Werror -c {} \;
+ar rc libft.a *.o
+ranlib libft.a
 find . -name "*.o" -type f -delete
